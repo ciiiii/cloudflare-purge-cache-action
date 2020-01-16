3414752ad118a9840a41ad1b1ab6a87ead94b9f4
ciiiii <allen.ccccnm@gmail.com>
2020-01-16 22:05:07 +0800

    handle error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core';
 import axios from 'axios';
 
+const baseUrl = 'https://api.cloudflare.com/client/v4';
+
 async function run() {
 	try {
 		const authToken = core.getInput('authToken', { required: true });
@@ -10,74 +12,63 @@ async function run() {
 		const fileList = !!rawFileList ? JSON.parse(rawFileList) : [];
 
 		if (fileList.length > 0) {
-		    const areFilesValid = fileList.reduce((acc, curr) => {
-		        if (!acc) {
-		            return false;
-                }
+			const areFilesValid = fileList.reduce((acc, curr) => {
+				if (!acc) {
+					return false;
+				}
+				return typeof curr === 'string' || curr.url;
+			}, true);
 
-		        return typeof curr === 'string' || curr.url;
-            }, true);
-
-		    if (!areFilesValid) {
-                core.setFailed('File list is formatted incorrectly. See https://api.cloudflare.com/#zone-purge-files-by-url for details');
-            }
-        }
-
-        const zoneID = await getZoneID(
-            authToken,
-            siteName,
-            authEmail
-        );
-        const response = await axios.post(
-            `https://api.cloudflare.com/client/v4/zones/${zoneID}/purge_cache`,
-            {
-                purge_everything: fileList.length === 0,
-                fileList: fileList.length === 0 ? undefined : fileList
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${authToken}`,
-                    'X-Auth-Email': authEmail
-                }
-            }
-        );
+			if (!areFilesValid) {
+				core.setFailed(
+					'File list is formatted incorrectly. See https://api.cloudflare.com/#zone-purge-files-by-url for details'
+				);
+			}
+		}
+		let zoneId = await getZoneID(authToken, authEmail, siteName);
+		let res = await axios.post(
+			`${baseUrl}/zones/${zoneId}/purge_cache`,
+			{
+				purge_everything: fileList.length === 0,
+				fileList: fileList.length === 0 ? undefined : fileList
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${authToken}`,
+					'X-Auth-Email': authEmail
+				}
+			}
+		);
+		let { success } = res.data;
+		if (success) {
+			core.setOutput('res', `Purge ${siteName} cache success!`);
+		}
+		core.setFailed(`Purge ${siteName} cache fail!`);
 	} catch (error) {
 		core.setFailed(error.message);
 	}
 }
 
-function getZoneID(
+async function getZoneID(
 	authToken: string,
-	siteName: string,
-	authEmail: string
-): Promise<string> {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await axios.get(
-				'https://api.cloudflare.com/client/v4/zones',
-				{
-					headers: {
-						Authorization: `Bearer ${authToken}`,
-						'X-Auth-Email': authEmail
-					}
-				}
-			);
-
-			const targetZone = response.data.result.find(
-				site => site.name === siteName
-			);
-
-			if (!targetZone) {
-				reject(
-					'Your zone was not found. Are you sure you passed the right name?'
-				);
+	authEmail: string,
+	siteName: string
+) {
+	try {
+		let res = await axios.get(`${baseUrl}/zones?name=${siteName}`, {
+			headers: {
+				Authorization: `Bearer ${authToken}`,
+				'X-Auth-Email': authEmail
 			}
-
-			resolve(targetZone.id);
-		} catch (e) {
-			reject(e);
+		});
+		let { result } = res.data;
+		if (result.length != 0) {
+			return result[0].id;
 		}
-	});
+		core.setFailed(`Query zoneId error, check your siteName: ${siteName}.`);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
 run();
